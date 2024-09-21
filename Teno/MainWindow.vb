8d6e44df872da06475ef26eababbadc76ec14534
Ernesto Vivas <ernesto.vivas@outlook.com>
2024-09-22 01:08:29 +0200

    Add search bar to search for songs

--- Teno/MainWindow.vb
+++ Teno/MainWindow.vb
@@ -4,6 +4,9 @@
 
         Me.Text = "Teno"
 
+
+        ' TOP MENU
+
         ' Create MenuStrip and menu items
         Dim menuStrip As New MenuStrip()
         Dim fileMenuItem As New ToolStripMenuItem("File")
@@ -20,10 +23,41 @@
         Me.MainMenuStrip = menuStrip
         Me.Controls.Add(menuStrip)
 
+
+        ' SEARCH BAR
+
+        ' Create and configure the SearchBar (TextBox=
+        Dim searchTextBox As New TextBox With {
+            .Name = "searchTextBox",
+            .Location = New Point(10, 50), ' Set location on the form
+            .Width = 330, ' Set width
+            .BorderStyle = BorderStyle.FixedSingle,
+            .Text = "With A Little Help From My Friends ...",
+            .ForeColor = Color.Gray
+        }
+
+        ' Create and configure the Button
+        Dim searchButton As New Button With {
+            .Text = "Buscar",
+            .Location = New Point(350, 50), ' Set location next to the TextBox
+            .BackColor = Color.LightBlue,
+            .FlatStyle = FlatStyle.Flat
+        }
+        AddHandler searchButton.Click, AddressOf SearchButton_Click ' Event handler for button click
+
+        ' Add the controls to the form
+        Me.Controls.Add(searchTextBox)
+        Me.Controls.Add(searchButton)
+
     End Sub
 
     Private Sub ExitMenuItem_Click(sender As Object, e As EventArgs)
         Me.Close() ' Closes the application
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs)
+        Dim searchText As String = CType(Me.Controls("searchTextBox"), TextBox).Text
+        MessageBox.Show("Searching for: " & searchText) ' Implement search logic here
     End Sub
 
 End Class
